afec1fd26d45ccda2b2deb82f40a5810b04fce65
Aman Prajapati <ap757162@gmail.com>
2025-03-04 10:29:01 +0530

    [Aman] Add. Functionality to calculate daily wage of employee added.

---
+++ EmployeeWageApplication/UC2_DailyEmployeeWage.js
@@ -0,0 +1,28 @@
+// Declare and Initialize the required constants 
+const WAGE_PER_HOUR = 20;
+const FULL_TIME_HOURS = 8;
+const PART_TIME_HOURS = 4;
+
+let workType = Math.floor(Math.random() * 3);
+let workHours = 0;
+
+// Calculate the daily wages of employee
+switch (workType) {
+    case 1:
+        workHours = PART_TIME_HOURS;
+        console.log("Employee worked Part Time");
+        break;
+    case 2:
+        workHours = FULL_TIME_HOURS;
+        console.log("Employee worked Full Time");
+        break;
+    default:
+        workHours = 0;
+        console.log("Employee did not work");
+        break;
+}
+
+let dailyWage = workHours * WAGE_PER_HOUR;
+
+// Display the result 
+console.log("Daily Wage: $" + dailyWage);
